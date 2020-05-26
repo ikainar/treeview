3caedfce1ca7756b1531f96c80ad728b67a92885
Кайнар Ильясов <kainar@macbook.local>
2020-05-27 00:15:42 +0600

    localStorage settings was added

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import { saveTreeItems, loadTreeItems } from "./localStorage";
+
+import "./index.css";
+import App from "./App";
+
+import { rootReducer } from "./redux";
+
+const store = createStore(rootReducer, {
+  treeItems: loadTreeItems(),
+  treeItemIdOnEditMode: null,
+});
+
+store.subscribe(async () => {
+  saveTreeItems(store.getState().treeItems);
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
